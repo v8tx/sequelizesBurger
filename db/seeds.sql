51f013b86dae69d68f7ea0822c58bfe7344eeb5f
Veronica Diaz <veronica.diaz@endurance.com>
2017-02-22 23:19:04 -0600

    updating files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheesy melt', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Madness', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Guac n roll', FALSE);
+
